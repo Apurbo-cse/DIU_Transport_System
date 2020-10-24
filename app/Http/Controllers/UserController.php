c096175f2c22eb7a64bc72ac8a57c34b793a09e9
mizanur <mizanurahmed1452@gmail.com>
2020-10-25 01:58:19 +0600

    User table create and store done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+            'image' => 'mimes:jpeg,png|max:8192',
+        ]);
+        $data = $request->except(['_token', 'password', 'image']);
+        $data['password'] = bcrypt($request->password);
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $path = 'images/user';
+            $file_name = $file->getClientOriginalName();
+            $file->move($path, $file_name);
+            $data['image'] = $path . '/' . $file_name;
+        }
+
+        User::create($data);
+        return redirect()->route('user.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/layouts/admin/_leftNav.blade.php
+++ resources/views/layouts/admin/_leftNav.blade.php
@@ -21,8 +21,8 @@
             <li class="has_sub">
                 <a href="javascript:void(0);" class="waves-effect"><i class="ti-user"></i> <span> Users </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
                 <ul class="list-unstyled">
-                    <li><a href="ui-buttons.html">User List</a></li>
-                    <li><a href="ui-panels.html">Create New User</a></li>
+                    <li><a href="{{route('user.index')}}">User List</a></li>
+                    <li><a href="{{route('user.create')}}">Create New User</a></li>
                 </ul>
             </li>
         </ul>
