d14363c5f80b101e0220f7b213fd43a63c22fe88
mizanur <mizanurahmed1452@gmail.com>
2020-11-10 19:36:34 +0600

    Slider Dinamic Done

--- resources/views/layouts/frontend/_slider.blade.php
+++ resources/views/layouts/frontend/_slider.blade.php
@@ -12,14 +12,16 @@
             <p>A New Journey in Campus Automate Transportation System</p>
         </div>
     </div>
+    @foreach($sliders as $slider)
     <div class="carousel-item">
-        <img src="{{asset('assets/frontend/images/bus13.jpg')}}" alt="Los Angeles" style="width: 100%; height:450px">
+        <img src="{{asset($slider->image)}}" alt="Los Angeles" style="width: 100%; height:450px">
         <div class="carousel-caption">
-            <h3 style="text-shadow: 6px solid red">Daffodil Internatinal University</h3>
-            <p>A New Journey in Campus Automate Transportation System</p>
+            <h3 style="text-shadow: 6px solid red">{{$slider->title}}</h3>
+            <p>{{$slider->description}}</p>
         </div>
     </div>
-    <div class="carousel-item">
+    @endforeach
+    {{--<div class="carousel-item">
         <img src="{{asset('assets/frontend/images/bus1.jpg')}}" alt="Chicago" style="width: 100%; height:400px">
         <div class="carousel-caption">
             <h3>DIU</h3>
@@ -32,7 +34,7 @@
             <h3>DIU</h3>
             <p>We love the Big Apple!</p>
         </div>
-    </div>
+    </div>--}}
 </div>
 <a class="carousel-control-prev" href="#demo" data-slide="prev">
     <span class="carousel-control-prev-icon"></span>
