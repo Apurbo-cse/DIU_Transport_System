d91d781283d218b6343d4cba11f285b2858a1554
mizanur <mizanurahmed1452@gmail.com>
2020-11-15 01:54:20 +0600

    Gallery CRUD Done

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -14,7 +14,9 @@ class GalleryController extends Controller
      */
     public function index()
     {
-        //
+        $data['galleries'] = Gallery::orderBy('created_at', 'DESC')->paginate(20);
+        $data['serial'] = 1;
+        return view('admin.gallery.index', $data);
     }
 
     /**
@@ -24,7 +26,7 @@ class GalleryController extends Controller
      */
     public function create()
     {
-
+        return view('admin.gallery.create');
     }
 
     /**
@@ -35,7 +37,30 @@ class GalleryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            'status' => 'required',
+
+        ]);
+
+        if ($request->hasFile('image')){
+            $file = $request->file('image');
+            $path ='images/gallery';
+            $file_name = time() . $file->getClientOriginalName();
+            $file->move($path, $file_name);
+            $data['image']= $path.'/'. $file_name;
+
+        }
+
+        $data['title'] = $request->title;
+        $data['description'] = $request->description;
+        $data['status'] = $request->status;
+
+        Gallery::create($data);
+        session()->flash('success', 'Gallery Create Successfully');
+        return redirect()->route('gallery.index');
     }
 
     /**
@@ -57,7 +82,8 @@ class GalleryController extends Controller
      */
     public function edit(Gallery $gallery)
     {
-        //
+        $data['gallery']=$gallery;
+        return view('admin.gallery.edit', $data);
     }
 
     /**
@@ -69,7 +95,34 @@ class GalleryController extends Controller
      */
     public function update(Request $request, Gallery $gallery)
     {
-        //
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'image' => 'image|mimes:jpeg,png,jpg|max:2048',
+            'status' => 'required',
+
+        ]);
+
+        if ($request->hasFile('image')){
+            $file = $request->file('image');
+            $path ='images/gallery';
+            $file_name = time() . $file->getClientOriginalName();
+            $file->move($path, $file_name);
+            $data['image']= $path.'/'. $file_name;
+
+            if (file_exists($gallery->image)){
+                unlink($gallery->image);
+            }
+        }
+
+        $data['title'] = $request->title;
+        $data['description'] = $request->description;
+        $data['status'] = $request->status;
+
+        $gallery->update($data);
+        session()->flash('success', 'Gallery Update Successfully');
+        return redirect()->route('gallery.index');
+
     }
 
     /**
@@ -80,6 +133,15 @@ class GalleryController extends Controller
      */
     public function destroy(Gallery $gallery)
     {
-        //
+        if($gallery){
+            if(file_exists(($gallery->image))){
+                unlink($gallery->image);
+            }
+
+            $gallery->delete();
+            session()->flash('success', 'Gallery deleted successfully');
+        }
+
+        return redirect()->route('gallery.index');
     }
 }
