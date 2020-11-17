a2ca08a02fe6ef6c46c5c888c67299f922c9d71a
mizanur <mizanurahmed1452@gmail.com>
2020-11-18 02:05:12 +0600

    Stuff CRUD Done

--- app/Http/Controllers/StuffController.php
+++ app/Http/Controllers/StuffController.php
@@ -89,7 +89,10 @@ class StuffController extends Controller
      */
     public function edit(Stuff $stuff)
     {
-        //
+        $data['bus_categories'] = Bus_Category::orderBy('name')->get();
+        $data['routes'] = Route::orderBy('route_name')->get();
+        $data['stuff'] = $stuff;
+        return view('admin.stuff.edit', $data);
     }
 
     /**
@@ -101,7 +104,32 @@ class StuffController extends Controller
      */
     public function update(Request $request, Stuff $stuff)
     {
-        //
+        $request->validate([
+            'category_id' => 'required',
+            'route_id' => 'required',
+            'bus_name' => "required|unique:stuffs,bus_name,$stuff->id",
+            'designation' => 'required',
+            'driver_name' => "required|unique:stuffs,driver_name,$stuff->id",
+            'driver_phone' => 'required|min:11|numeric',
+            'helper_name' => "required|unique:stuffs,helper_name,$stuff->id",
+            'helper_phone' => 'required|min:11|numeric',
+            'status' => 'required',
+        ]);
+
+        $data['category_id'] = $request->category_id;
+        $data['route_id'] = $request->route_id;
+        $data['bus_name'] = $request->bus_name;
+        $data['designation'] = $request->designation;
+        $data['driver_name'] = $request->driver_name;
+        $data['driver_phone'] = $request->driver_phone;
+        $data['helper_name'] = $request->helper_name;
+        $data['helper_phone'] = $request->helper_phone;
+        $data['status'] = $request->status;
+
+
+        $stuff->update($data);
+        session()->flash('success', 'Stuff Update Successfully');
+        return redirect()->route('stuff.index');
     }
 
     /**
@@ -112,6 +140,8 @@ class StuffController extends Controller
      */
     public function destroy(Stuff $stuff)
     {
-        //
+        $stuff->delete();
+        session()->flash('success', 'Stuff Deleted Successfully');
+        return redirect()->route('stuff.index');
     }
 }
--- resources/views/admin/stuff/edit.blade.php
+++ resources/views/admin/stuff/edit.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.admin.master')
-@section('title', 'Update Post')
+@section('title', 'Update Stuff')
 @section('content')
     <!-- Page-Title -->
     <div class="row">
         <div class="col-sm-12">
             <div class="page-header-title">
-                <h4 class="pull-left page-title">Update Post</h4>
+                <h4 class="pull-left page-title">Update Stuff</h4>
                 <ol class="breadcrumb pull-right">
                     <li><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
-                    <li class="active">Update Post</li>
+                    <li class="active">Update Stuff</li>
                 </ol>
                 <div class="clearfix"></div>
             </div>
@@ -17,70 +17,102 @@
     <div class="row">
         <div class="col-sm-12">
             <div class="panel panel-primary">
-                <div class="panel-heading"><h3 class="panel-title">Post Form</h3></div>
+                <div class="panel-heading"><h3 class="panel-title">Stuff Form</h3></div>
                 <div class="panel-body">
-                    <form class="form-horizontal" action="{{route('post.update', $post->id)}}" method="post" enctype="multipart/form-data">
+                    <form class="form-horizontal" action="{{route('stuff.update', $stuff->id)}}" method="post" enctype="multipart/form-data">
                         @csrf
                         @method('put')
                         <div class="form-group">
-                            <label class="col-md-2 control-label">Post Title</label>
-                            <div class="col-md-10">
-                                <input value="{{$post->title}}" name="title" type="text" id="title" class="form-control" placeholder="Post title">
-                                @error('title')
+                            <label class="col-sm-2 control-label">Bus Category</label>
+                            <div class="col-sm-10">
+                                <select name="category_id" id="category_id" class="form-control">
+                                    <option value="" style="display: none" selected>Select Category</option>
+                                    @foreach($bus_categories as $category)
+                                        <option value="{{ $category->id }}" @if($stuff->category_id == $category->id) selected @endif> {{ $category->name }} </option>
+                                    @endforeach
+                                </select>
+                                @error('category_id')
                                 <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-md-2 control-label">Post Description</label>
-                            <div class="col-md-10">
-                                <textarea name="description" class="form-control" rows="5" placeholder="Post Description">{{$post->description}}</textarea>
-                                @error('description')
+                            <label class="col-sm-2 control-label">Bus Route</label>
+                            <div class="col-sm-10">
+                                <select name="route_id" id="route_id" class="form-control">
+                                    <option value="" style="display: none" selected>Select Route</option>
+                                    @foreach($routes as $route)
+                                        <option value="{{ $route->id }}" @if($stuff->route_id == $route->id) selected @endif> {{ $route->route_name }} </option>
+                                    @endforeach
+                                </select>
+                                @error('route_id')
                                 <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group">
-                            <label class="col-md-2 control-label">Choose Tags</label>
+                            <label class="col-md-2 control-label">Bus Name</label>
                             <div class="col-md-10">
-                                @foreach($tags as $tag)
-                                    <div class="checkbox checkbox-inline">
-                                        <input name="tags[]" type="checkbox" id="tag{{ $tag->id}}" value="{{ $tag->id }}"
-                                           @foreach($post->tags as $t)
-                                                  @if($tag->id == $t->id) checked @endif
-                                            @endforeach
-                                        >
-                                        <label for="tag{{ $tag->id}}"> {{ $tag->name }} </label>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">Featured</label>
-                            <div class="col-md-10">
-                                <div class="checkbox checkbox-success">
-                                    <input @if($post->is_featured == 1) checked @endif  value="1" type="checkbox" class="form-check-input" name="is_featured" id="is_featured">
-                                    <label for="checkbox">
-                                        Yes
-                                    </label>
-                                </div>
-                                @error('is_featured')
+                                <input value="{{$stuff->bus_name}}" name="bus_name" type="text" id="bus_name" class="form-control" placeholder="Bus name">
+                                @error('bus_name')
                                 <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group">
-                            <label class="col-md-2 control-label">Post Status</label>
+                            <label class="col-md-2 control-label">Designation</label>
+                            <div class="col-md-10">
+                                <input value="{{$stuff->designation}}" name="designation" type="text" id="designation" class="form-control" placeholder="Designation">
+                                @error('designation')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">Driver Name</label>
+                            <div class="col-md-10">
+                                <input value="{{$stuff->driver_name}}" name="driver_name" type="text" id="driver_name" class="form-control" placeholder="Driver name">
+                                @error('driver_name')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">Driver Phone No</label>
+                            <div class="col-md-10">
+                                <input value="{{$stuff->driver_phone}}" name="driver_phone" type="tel" id="driver_phone" class="form-control" placeholder="017xxxxxxxx">
+                                @error('driver_phone')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">Helper Name</label>
+                            <div class="col-md-10">
+                                <input value="{{$stuff->helper_name}}" name="helper_name" type="text" id="helper_name" class="form-control" placeholder="Helper name">
+                                @error('helper_name')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">Helper Phone No</label>
+                            <div class="col-md-10">
+                                <input value="{{$stuff->helper_phone}}" name="helper_phone" type="tel" id="helper_phone" class="form-control" placeholder="017xxxxxxxx">
+                                @error('helper_phone')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">Stuff Status</label>
                             <div class="col-md-10">
                                 <div class="radio radio-info radio-inline">
-                                    <input @if($post->status == 'active') checked   @endif type="radio" id="active" value="active" name="status">
+                                    <input @if($stuff->status == 'active') checked   @endif type="radio" id="active" value="active" name="status">
                                     <label for="active"> Active </label>
                                 </div>
                                 <div class="radio radio-inline">
-                                    <input @if($post->status == 'inactive') checked   @endif type="radio" id="inactive" value="inactive" name="status">
+                                    <input @if($stuff->status == 'inactive') checked   @endif type="radio" id="inactive" value="inactive" name="status">
                                     <label for="inactive"> Inactive </label>
                                 </div>
                                 @error('status')
@@ -88,23 +120,9 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">Post Image</label>
-                            <div class="col-md-10">
-                                <input type="file" class="form-control" name="image" >
-                                @error('image')
-                                <div class="text-danger">{{ $message }}</div>
-                                @enderror
-                                @if($post->image != null)
-                                    <img src="{{ asset($post->image) }}" width="20%">
-                                @endif
-                            </div>
-                        </div>
                         <div class="pull-right">
                             <button type="submit" class="btn btn-info waves-effect waves-light">Update</button>
                         </div>
-
-
 
                     </form>
                 </div> <!-- panel-body -->
