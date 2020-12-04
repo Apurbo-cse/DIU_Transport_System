399eaca30aaa1d87ddc563ca8c7e9cd501085e70
mizanur <mizanurahmed1452@gmail.com>
2020-12-05 02:26:54 +0600

    Student Registration Done

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -1,77 +1,12 @@
-{{--
 <!DOCTYPE html>
 <html>
-
-<head>
-    <title>Login :: DIU_Transport_System</title>
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/style/loginregistration.css')}}">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="{{asset('assets/frontend/icons/fav-icon.png')}}" />
-</head>
-
-<body>
-<img class="wave" src="{{asset('assets/frontend/images/wave.png')}}">
-<div class="container">
-    <div class="img">
-        <img src="{{asset('assets/frontend/images/as.png')}}" style="width: 290px;">
-    </div>
-    <div class="login-content">
-        <form action="{{route('login')}}" method="post">
-            @csrf
-            <img src="{{asset('assets/frontend/images/logo.png')}}" style="width: 70%">
-            <h2 class="title">DIU Transport</h2>
-            <div class="input-div one">
-                <div class="i">
-                    <i class="fas fa-user"></i>
-                </div>
-                <div class="div">
-                    <h5>Email</h5>
-                    <input id="email" type="email" class="input form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email" autofocus >
-                </div>
-            </div>
-            @error('email')
-            <div style="color: red;">{{ $message }}</div>
-            @enderror
-
-            <div class="input-div pass">
-                <div class="i">
-                    <i class="fas fa-lock"></i>
-                </div>
-                <div class="div">
-                    <h5>Password</h5>
-                    <input id="password" type="password" class="input form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password" >
-                </div>
-            </div>
-            @error('password')
-            <div style="color: red;">{{ $message }}</div>
-            @enderror
-            <div class="check">
-                <input type="checkbox" id="vehicle1" name="vehicle1" style="margin-left: -250px;margin-top:15px;">
-                <label for="vehicle1" style=" color: #999;
-                    font-size: 0.9rem; "> I have a bike</label><br>
-                <a href="{{route('student.registration')}}">Registration</a>
-            </div>
-
-            <input type="submit" class="btn" value="Login">
-        </form>
-    </div>
-</div>
-<script type="text/javascript" src="{{asset('assets/frontend/js/loginregistration.js')}}"></script>
-</body>
-
-</html>
---}}
-<!DOCTYPE html>
-<html>
-
 <head>
     <title>Login :: DIU_Transport_System</title>
     <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/style/lon.css')}}">
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{asset('assets/frontend/icons/fav-icon.png')}}" />
     <link rel="stylesheet" href="{{asset('assets/frontend/style/lonc.css')}}">
 </head>
 
--- resources/views/frontend/registration.blade.php
+++ resources/views/frontend/registration.blade.php
@@ -3,112 +3,89 @@
 
 <head>
     <title>Registration :: DIU_Transport_System</title>
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/style/loginregistration.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/style/lon.css')}}">
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="{{asset('assets/frontend/icons/fav-icon.png')}}" />
-
+    <link rel="stylesheet" href="{{asset('assets/frontend/style/lonc.css')}}">
 </head>
 
 <body>
 <img class="wave" src="{{asset('assets/frontend/images/wave.png')}}">
 <div class="container">
     <div class="img">
-        <img src="{{asset('assets/frontend/images/as.png')}}" style="width: 290px;">
+        <img src="{{asset('assets/frontend/images/as.png')}}" style="width: 289px;">
     </div>
+
     <div class="login-content">
-        <form method="POST" action="{{ route('register') }}">
+
+        <form action="{{ route('register') }}" method="POST" class="sign-in-form">
             @csrf
 
-{{--
-            <img src="{{asset('assets/frontend/images/logo.png')}}">
---}}
-            <h2 class="title">DIU Transport</h2>
-            <div class="input-div one">
-                <div class="i">
-                    <i class="fas fa-user"></i>
-                </div>
-                <div class="div">
-                    <h5>Username</h5>
-                    <input type="text" name="name" class="input from-control @error('name') is-invalid @enderror" value="{{ old('name') }}"  autocomplete="name" autofocus />
-                </div>
-            </div>
-            @error('name')
-            <div style="color: red;">{{ $message }}</div>
-            @enderror
-            <div class="input-div one">
-                <div class="i">
-                    <i class="fas fa-user"></i>
-                </div>
-                <div class="div">
-                    <h5>ID No</h5>
-                    <input type="text" name="user_id" class="input from-control @error('user_id') is-invalid @enderror" value="{{ old('user_id') }}" autocomplete="user_id" autofocus  />
-                </div>
-            </div>
-            @error('user_id')
-            <div style="color: red;">{{ $message }}</div>
-            @enderror
 
-            <div class="input-div one">
-                <div class="i">
-                    <i class="fas fa-user"></i>
-                </div>
-                <div class="div">
-                    <h5>Department</h5>
-                    <input type="text" name="department" class="input from-control @error('department') is-invalid @enderror" value="{{ old('department') }}" autocomplete="department" autofocus />
-                </div>
+            <img src="{{asset('assets/frontend/images/profilepic.png')}}">
+            <h2 class="login-title">User Info</h2>
+            <div class="login-input-field">
+                <i class="fas fa-user"></i>
+                <input type="text" name="name" class="from-control @error('name') is-invalid @enderror" value="{{ old('name') }}"  autocomplete="name" autofocus placeholder="Username" />
             </div>
-            @error('department')
-            <div style="color: red;">{{ $message }}</div>
-            @enderror
+                @error('name')
+                    <div style="color: red;">{{ $message }}</div>
+                @enderror
 
-            <div class="input-div one">
-                <div class="i">
-                    <i class="fas fa-user"></i>
-                </div>
-
-                <div class="div">
-                    <h5>Email</h5>
-                    <input type="email" name="email" class="input from-control @error('email') is-invalid @enderror" value="{{ old('email') }}" autocomplete="email" autofocus />
-                </div>
+            <div class="login-input-field">
+                <i class="fas fa-id-card"></i>
+                <input type="text" name="user_id" class="from-control @error('user_id') is-invalid @enderror" value="{{ old('user_id') }}" autocomplete="user_id" autofocus placeholder="171-15-XXX" />
             </div>
-            @error('email')
-            <div style="color: red;">{{ $message }}</div>
-            @enderror
+                @error('user_id')
+                    <div style="color: red;">{{ $message }}</div>
+                @enderror
 
-            <div class="input-div one">
-                <div class="i">
-                    <i class="fas fa-user"></i>
-                </div>
-                <div class="div">
-                    <h5>01714-90XXX</h5>
-                    <input type="text" name="phone_no" class="input from-control @error('phone_no') is-invalid @enderror" value="{{ old('phone_no') }}" autocomplete="phone_no" autofocus />
-                </div>
+            <div class="login-input-field">
+                <i class="fas fa-graduation-cap"></i>
+                <input type="text" name="department" class="from-control @error('department') is-invalid @enderror" value="{{ old('department') }}" autocomplete="department" autofocus placeholder="CSE" />
             </div>
-            @error('phone_no')
-            <div style="color: red;">{{ $message }}</div>
-            @enderror
+                @error('department')
+                    <div style="color: red;">{{ $message }}</div>
+                @enderror
 
-            <div class="input-div pass">
-                <div class="i">
-                    <i class="fas fa-lock"></i>
-                </div>
-                <div class="div">
-                    <h5>Password</h5>
-                    <input type="password" name="password" class="input from-control @error('password') is-invalid @enderror" value="{{ old('password') }}" autocomplete="password" autofocus  />
-                </div>
+            <div class="login-input-field">
+                <i class="fas fa-phone"></i>
+                <input type="text" name="phone_no" class="from-control @error('phone_no') is-invalid @enderror" value="{{ old('phone_no') }}" autocomplete="phone_no" autofocus placeholder="+88017XXXXXXXX" />
             </div>
-            @error('password')
-            <div style="color: red;">{{ $message }}</div>
-            @enderror
+                @error('phone_no')
+                    <div style="color: red;">{{ $message }}</div>
+                @enderror
 
-            <input type="submit" class="btn" value="SIGNUP">
+            <div class="login-input-field">
+                <i class="fas fa-envelope"></i>
+                <input type="email" name="email" class="from-control @error('email') is-invalid @enderror" value="{{ old('email') }}" autocomplete="email" autofocus placeholder="abc15-14XX@diu.edu.bd" />
+            </div>
+                @error('email')
+                    <div style="color: red;">{{ $message }}</div>
+                @enderror
+
+            <div class="login-input-field">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="password" class="from-control @error('password') is-invalid @enderror" value="{{ old('password') }}" autocomplete="password" autofocus placeholder="Password" />
+            </div>
+                @error('password')
+                    <div style="color: red;">{{ $message }}</div>
+                @enderror
+
+            <button name="signup-btn" class="btn">signup</button>
+            <br><br> (
+            <a href="{{route('student.login')}}" style="color: red;
+                   font-size: 0.9rem;"> Login</a>)
+
         </form>
     </div>
 </div>
-<script type="text/javascript" src="{{asset('assets/frontend/js/loginregistration.js')}}"></script>
-
+<script type="text/javascript" src="{{asset('assets/frontend/js/lonc.js')}}"></script>
 </body>
 
 </html>
+
+
+
